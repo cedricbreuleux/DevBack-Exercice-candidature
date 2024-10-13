df139f2b9e7fb01672e50cc0144a3f78c3df9c19
cedricbreuleux <cedric.breuleux@epitech.eu>
2024-10-13 15:21:11 +0200

    fix architecture
    Suppression fichier inutile

--- api-platform-3.3.7/pwa/pages/_app.jsx
+++ api-platform-3.3.7/pwa/pages/_app.jsx
@@ -1,11 +1,11 @@
-import "../styles/globals.css"
-import Layout from "../components/common/Layout"
+import "../styles/globals.css";
+import Layout from "../components/common/Layout";
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout dehydratedState={pageProps.dehydratedState}>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    return (
+        <Layout dehydratedState={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
-export default MyApp
+export default MyApp;
